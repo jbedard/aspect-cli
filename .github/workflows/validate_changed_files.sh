88a1c20f24af2fe837e8e4857c98b8fb9a716b5a
Jason Bedard <jason@aspect.dev>
2022-02-02 17:58:27 -0800

    build: allow PRs from forks

--- .github/workflows/validate_changed_files.sh
+++ .github/workflows/validate_changed_files.sh
@@ -7,10 +7,11 @@ VALID_FILE_PATHS=('pkg/plugin/sdk/v1alpha2') # Array of filepaths that are allow
 
 # by default the only local branch will be pull/PR#/merge
 # fetch only the latest commit from the 2 branches in question to avoid fetching the entire repo which could be costly
-git fetch --depth 1 origin "$1"
-git fetch --depth 1 origin "$2"
+git remote add from-repo "$1"
+git fetch --depth 1 from-repo "$2"
+git fetch --depth 1 from-repo "$3"
 
-git diff "origin/$1..origin/$2" --name-only | while read -r file; do
+git diff "from-repo/$2..origin/$3" --name-only | while read -r file; do
 
     # check if filepath matches a valid path. If so move to the next change
     for valid_path in "${VALID_FILE_PATHS[@]}"; do
